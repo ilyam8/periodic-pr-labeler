04c735248510deff21512c9f200e20fec21ab88b
ilyam8 <ilya@netdata.cloud>
2020-02-09 22:20:42 +0300

    minor

--- cmd/labeler/main.go
+++ cmd/labeler/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"errors"
 	"os"
+	"strings"
 
 	"github.com/ilyam8/periodic-pr-labeler/pkg/labeling"
 	"github.com/ilyam8/periodic-pr-labeler/pkg/mappings"
@@ -15,13 +17,26 @@ type options struct {
 	RepoSlug           string `short:"r" long:"repository" description:"github repository slug"`
 	Token              string `short:"t" long:"token" description:"github token"`
 	LabelMappings      string `short:"m" long:"label-mappings" description:"label mappings file on github"`
-	LocalLabelMappings string `short:"M" long:"label-mappings-local" description:"label mappings file on the local system"`
+	LabelMappingsLocal string `short:"M" long:"label-mappings-local" description:"label mappings file on the local system"`
 	DryRun             bool   `short:"d" long:"dry-run" description:"dry run, labels won't be applied, only reported"`
 }
 
+func validateOptions(opts options) error {
+	if opts.RepoSlug == "" {
+		return errors.New("repository slug config parameter not set")
+	}
+	if opts.Token == "" {
+		return errors.New("token config parameter not set")
+	}
+	if opts.LabelMappingsLocal == "" && opts.LabelMappings == "" {
+		return errors.New("label mappings config parameter not set")
+	}
+	return nil
+}
+
 func parseCLI() options {
-	var opt options
-	parser := flags.NewParser(&opt, flags.Default)
+	var opts options
+	parser := flags.NewParser(&opts, flags.Default)
 	parser.Name = "periodic-pr-labeler"
 	parser.Usage = "[OPTION]..."
 
@@ -31,7 +46,7 @@ func parseCLI() options {
 		}
 		os.Exit(1)
 	}
-	return opt
+	return opts
 }
 
 func applyFromEnv(opts *options) {
@@ -46,17 +61,33 @@ func applyFromEnv(opts *options) {
 	}
 }
 
+func extractOwnerName(repoSlug string) (owner, name string, ok bool) {
+	parts := strings.Split(strings.TrimSpace(repoSlug), "/")
+	if len(parts) != 2 || parts[0] == "" || parts[1] == "" {
+		return "", "", false
+	}
+	return parts[0], parts[1], true
+}
+
 func newRepositoryService(opts options) *repository.Repository {
-	conf := repository.Config{}
+	owner, name, ok := extractOwnerName(opts.RepoSlug)
+	if !ok {
+		log.Fatalf("repository slug config parameter bad syntax ('%s')", opts.RepoSlug)
+	}
+	conf := repository.Config{
+		Owner: owner,
+		Name:  name,
+		Token: opts.Token,
+	}
 	return repository.New(conf)
 }
 
-func newMappingsService(opts options, repo *repository.Repository) (ms *mappings.Mappings) {
+func newMappingsService(opts options, rs *repository.Repository) (ms *mappings.Mappings) {
 	var err error
-	if opts.LocalLabelMappings != "" {
-		ms, err = mappings.FromFile(opts.LocalLabelMappings)
+	if opts.LabelMappingsLocal != "" {
+		ms, err = mappings.FromFile(opts.LabelMappingsLocal)
 	} else {
-		ms, err = mappings.FromGitHub(opts.LocalLabelMappings, repo)
+		ms, err = mappings.FromGitHub(opts.LabelMappings, rs)
 	}
 	if err != nil {
 		log.Fatal(err)
@@ -64,9 +95,21 @@ func newMappingsService(opts options, repo *repository.Repository) (ms *mappings
 	return ms
 }
 
+func newLabelingService(rs *repository.Repository, ms *mappings.Mappings, opts options) *labeling.Labeler {
+	labSvc := labeling.New(rs, ms)
+	if opts.DryRun {
+		labSvc.DryRun = true
+	}
+	return labSvc
+}
+
 func main() {
 	opts := parseCLI()
 	applyFromEnv(&opts)
+
+	if err := validateOptions(opts); err != nil {
+		log.Fatal(err)
+	}
 
 	if opts.DryRun {
 		log.SetLevel(log.DebugLevel)
@@ -74,7 +117,7 @@ func main() {
 
 	repoSvc := newRepositoryService(opts)
 	mapSvc := newMappingsService(opts, repoSvc)
-	labSvc := labeling.New(repoSvc, mapSvc)
+	labSvc := newLabelingService(repoSvc, mapSvc, opts)
 
 	if err := labSvc.ApplyLabels(); err != nil {
 		log.Fatal(err)
--- pkg/repository/repository.go
+++ pkg/repository/repository.go
@@ -3,7 +3,6 @@ package repository
 import (
 	"context"
 	"fmt"
-	"strings"
 
 	"github.com/google/go-github/v29/github"
 	"golang.org/x/oauth2"
@@ -13,14 +12,6 @@ func newGitHubClient(token string) *github.Client {
 	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})
 	tc := oauth2.NewClient(context.Background(), ts)
 	return github.NewClient(tc)
-}
-
-func extractOwnerName(repoSlug string) (owner, name string, ok bool) {
-	parts := strings.Split(strings.TrimSpace(repoSlug), "/")
-	if len(parts) != 2 || parts[0] == "" || parts[1] == "" {
-		return "", "", false
-	}
-	return parts[0], parts[1], true
 }
 
 func New(conf Config) *Repository {
@@ -54,7 +45,7 @@ func (r Repository) Name() string {
 func (r Repository) FileContent(filePath string) (*github.RepositoryContent, error) {
 	content, _, _, err := r.Repositories.GetContents(context.TODO(), r.Owner(), r.Name(), filePath, nil)
 	if content == nil && err == nil {
-		err = fmt.Errorf("%s is not a file", filePath)
+		err = fmt.Errorf("'%s' is not a file", filePath)
 	}
 	return content, err
 }
